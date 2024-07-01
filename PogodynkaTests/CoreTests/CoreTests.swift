c5157f94a2eb77a4c9d14910ba2077f83ba47a23
Rafal Korzynski <rafal.korzynski@nextory.com>
2024-07-01 18:29:21 +0200

    Improved Unit tests for MapViewDecorator - fixed unit test bug - implemented Unit Test verification for region - improved CoreTests definitions

--- PogodynkaTests/CoreTests/CoreTests.swift
+++ PogodynkaTests/CoreTests/CoreTests.swift
@@ -10,20 +10,25 @@ import XCTest
 @testable import Pogodynka
 
 struct CoreTests {
+    
     struct NSErrors {
         static let unknown = NSError(domain: "Unknown domain - UnitTest", code: 999, userInfo: [NSLocalizedDescriptionKey: "Unknown description"])
         static let generalError = NSError(domain: "UnitTest.Error", code: 111, userInfo: [NSLocalizedDescriptionKey: "General Error used for unit testing"])
     }
     
+    // Define constants for sample cities
+    static let wroclawCity = (name: "Wrocław", lat: 51.111532963657154, lon: 17.03922093191807, country: "PL", state: "Dolnośląskie")
+    static let dabrowaCity = (name: "Dąbrowa", lat: 53.40802735, lon: 20.2072225551886, country: "PL", state: "Warmińsko Mazurskie")
+    
     struct LocationInfos {
-        static let wroclaw = LocationInfo(name: "Wrocław", lat: 50.96, lon: 16.96, country: "PL", state: "Dolnośląskie")
-        static let dabrowa = LocationInfo(name: "Dąbrowa", lat: 53.40802735, lon: 20.2072225551886, country:"PL", state: "Warmińsko Mazurskie")
+        static let wroclaw = LocationInfo(name: wroclawCity.name, lat: wroclawCity.lat, lon: wroclawCity.lon, country: "PL", state: "Dolnośląskie")
+        static let dabrowa = LocationInfo(name: dabrowaCity.name, lat: dabrowaCity.lat, lon: dabrowaCity.lon, country:"PL", state: "Warmińsko Mazurskie")
         static let locations = [wroclaw, dabrowa]
     }
     
     struct CityModels {
-        static let wroclaw = CityModel(name: "Wrocław", lat: 50.96, lon: 16.96, state: "Dolnośląskie", isPL: true)
-        static let dabrowa = CityModel(name: "Dąbrowa", lat: 53.40802735, lon: 20.2072225551886, state: "Warmińsko Mazurskie", isPL: true)
+        static let wroclaw = CityModel(name: "Wrocław", lat: wroclawCity.lat, lon: wroclawCity.lon, state: "Dolnośląskie", isPL: true)
+        static let dabrowa = CityModel(name: dabrowaCity.name, lat: dabrowaCity.lat, lon: dabrowaCity.lon, state: "Warmińsko Mazurskie", isPL: true)
     }
     
     struct WeatherInfos {
@@ -31,6 +36,11 @@ struct CoreTests {
     }
     struct ForecastInfos {
         static let wroclawForecast: ForecastInfo = decodeObject(fileName: "forecast_info")
+    }
+    
+    struct MapPoints {
+        static let wroclaw = MapPoint(lat: wroclawCity.lat, lon: wroclawCity.lon, title: wroclawCity.name)
+        static let dabrowa = MapPoint(lat: dabrowaCity.lat, lon: dabrowaCity.lon, title: wroclawCity.name)
     }
 }
 
--- PogodynkaTests/MapViewDecoratorTests.swift
+++ PogodynkaTests/MapViewDecoratorTests.swift
@@ -13,12 +13,12 @@ import Nimble
 
 final class MapViewDecoratorTests: XCTestCase {
 
-    var mapView: MKMapView!
+    var mapView: MKMapViewStub!
     var decorator: MapViewDecorator!
     
     override func setUp() {
         super.setUp()
-        mapView = MKMapView()
+        mapView = MKMapViewStub()
         decorator = MapViewDecorator(mapView: mapView)
     }
     
@@ -28,13 +28,24 @@ final class MapViewDecoratorTests: XCTestCase {
         super.tearDown()
     }
     
+    // MARK: - Helpers
+    
+    func equalRegions(_ reg1: MKCoordinateRegion, _ reg2: MKCoordinateRegion) -> Bool {
+        return reg1.center.latitude == reg2.center.latitude &&
+            reg1.center.longitude == reg2.center.longitude &&
+            reg1.span.latitudeDelta == reg2.span.latitudeDelta &&
+            reg1.span.longitudeDelta == reg2.span.longitudeDelta
+    }
+    
+    // MARK: - Tests
+    
     func test_AddMapPointToMapView() {
         let wroclaw = CoreTests.LocationInfos.wroclaw
         let mapPoint = MapPoint(lat: wroclaw.lat, lon: wroclaw.lon, title: wroclaw.name)
         
         decorator.add(mapPoint: mapPoint)
         
-        expect(self.mapView.annotations).to(beEmpty())
+        expect(self.mapView.annotations).toNot(beEmpty())
         
         let annotation = mapView.annotations.first as? MKPointAnnotation
         expect(annotation?.title).to(equal(wroclaw.name))
@@ -51,5 +62,33 @@ final class MapViewDecoratorTests: XCTestCase {
         decorator.removeMapPoint()
         
         expect(self.mapView.annotations).to(beEmpty())
+    }
+    
+    func test_AddAndRemoveSequence_PreservesRegionValue() {
+        let dabrowa = CoreTests.MapPoints.dabrowa
+        
+        let initalRegion = self.mapView.region
+                
+        decorator.add(mapPoint: dabrowa)
+        
+        let isDabrowaRegionSet = equalRegions(mapView.updatedRegion, dabrowa.region)
+        expect(isDabrowaRegionSet).to(beTrue())
+        
+        let regionIsEqualAfterAdd = equalRegions(mapView.updatedRegion, initalRegion)
+        expect(regionIsEqualAfterAdd).to(beFalse())
+        
+        decorator.removeMapPoint()
+        
+        let regionIsEqualAfterRemove = equalRegions(mapView.region, initalRegion)
+        expect(regionIsEqualAfterRemove).to(beTrue())
+    }
+}
+
+class MKMapViewStub: MKMapView {
+    var updatedRegion = MKCoordinateRegion()
+        
+    override func setRegion(_ region: MKCoordinateRegion, animated: Bool) {
+        updatedRegion = region
+        super.setRegion(region, animated: animated)
     }
 }
